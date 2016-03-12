95102c57b446c2de9da93742cda04af5b31c9b10
Alex Paz <alexjpaz@gmail.com>
2016-03-11 19:12:51 -0500

    angluar

---
+++ web/app/index.js
@@ -0,0 +1,3 @@
+var angular = require('angularjs');
+
+console.log('hi');
---
+++ web/webpack.config.js
@@ -0,0 +1,28 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './app/index',
+  output: {
+    path: __dirname + '/public',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      riot: 'riot'
+    })
+  ],
+  module: {
+    preLoaders: [
+      { test: /\.tag$/, exclude: /node_modules/, loader: 'riotjs-loader', query: { type: 'none' } }
+    ],
+    loaders: [
+//      { test: /\.css$/, loader: "style-loader!css-loader" },
+  //    { test: /\.png$/, loader: "url-loader?limit=100000" },
+    //  { test: /\.jpg$/, loader: "file-loader" },
+      { test: /\.js$|\.tag$/, exclude: /node_modules/, loader: 'babel-loader' }
+    ]
+  },
+  devServer: {
+    contentBase: './public'
+  }
+};
