12748cd86c3decd2d467ba4944d9cacaf42e205a
Alexander Paz <alexjpaz@users.noreply.github.com>
2016-10-01 12:14:57 -0400

    Update Table.js

--- web/test/models/Table.js
+++ web/test/models/Table.js
@@ -10,4 +10,15 @@ describe('model/Table', () => {
     expect(t.rows.length).to.equal(1);
     expect(t.rows[0].list[0]).to.equal(3);
   });
+
+  it('should build a table with fractions', () => {
+    var t = new Table({
+      week: '3x5',
+      weight: 370
+    });
+    expect(t.rows.length).to.equal(1);
+    expect(t.rows[2].list[0]).to.equal(3);
+  });
 });
+
+
