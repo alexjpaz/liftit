7aa8b901327854e5dd073319b39025b7dfaa44e1
Alexander Paz <alexjpaz@gmail.com>
2018-04-08 07:48:06 -0400

    Show trends in cycles

---
+++ packages/web/src/common/Constants.js
@@ -0,0 +1,1 @@
+export const lifts = ['press','deadlift','bench','squat'];
--- packages/web/src/styles.js
+++ packages/web/src/styles.js
@@ -1,2 +1,14 @@
 import 'bulma/css/bulma.css'
 import './index.css';
+
+(function() {
+  if(document) {
+    const script = document.createElement('script');
+
+    script.src = 'https://use.fontawesome.com/releases/v5.0.9/js/all.js';
+
+    const root = document.head || document.body;
+
+    root.appendChild(script);
+  }
+})();
