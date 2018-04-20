a9042cd20654693bc061c5166e08216d003d0578
Alexander Paz <alexjpaz@gmail.com>
2018-04-20 10:29:07 -0400

    Added test to ensure that the key property doens't muck with things

--- packages/web/src/CycleDetail/CycleDetail.spec.js
+++ packages/web/src/CycleDetail/CycleDetail.spec.js
@@ -3,6 +3,7 @@ import React from 'react';
 import CycleDetail from './';
 
 import { 
+  mount,
   shallow
 } from 'enzyme';
 
@@ -17,7 +18,7 @@ describe('CycleDetail', () => {
       lift: 'press',
       weight: 85
     };
-    const wrapper = shallow(<CycleDetail 
+    const wrapper = mount(<CycleDetail 
       cycle={{
         type: 'cycle',
         date: '2018-01-01', 
@@ -28,5 +29,35 @@ describe('CycleDetail', () => {
 
     const table = wrapper.instance().generateTable();
     expect(table.press['85']).toEqual(log);
+
+    expect(wrapper.find('table').length).toEqual(1);
+    expect(wrapper.find('.table').length).toEqual(1);
+  });
+
+  it('should generate a table and have unique cells', () => {
+    // This is a test to ensure that the "key" property is set correctly 
+    // in the dom tree and the table displays correctly
+    //
+    const log = {
+      type: 'log',
+      lift: 'press',
+      weight: 85
+    };
+    const wrapper = mount(<CycleDetail 
+      cycle={{
+        type: 'cycle',
+        date: '2018-01-01', 
+        press: 100,
+      }}
+      logs={[log]}
+      />);
+
+    const table = wrapper.instance().generateTable();
+    expect(table.press['85']).toEqual(log);
+
+    expect(wrapper.find('table').length).toEqual(1);
+    expect(wrapper.find('.table').length).toEqual(1);
+    expect(wrapper.find('.fas').length).toEqual(12);
+    expect(wrapper.find('.fas.fa-check').length).toEqual(1);
   });
 });
