2f3bf0ba4a4d87f45d32786451abf33a57d3d3cb
Alexander Paz <alexjpaz@gmail.com>
2018-08-25 15:52:01 -0400

    working on the calender

--- packages/web/src/Calendar/WeekHeader.js
+++ packages/web/src/Calendar/WeekHeader.js
@@ -3,14 +3,14 @@ import React, { Component } from 'react';
 export default class WeekHeader extends Component { 
   render() {
     return (
-      <div className="columns has-text-centered is-gapless">
-        <div className="column">S</div>
-        <div className="column">M</div>
-        <div className="column">T</div>
-        <div className="column">W</div>
-        <div className="column">T</div>
-        <div className="column">F</div>
-        <div className="column">S</div>
+      <div className="WeekHeader">
+        <div className="WeekHeader__Column">S</div>
+        <div className="WeekHeader__Column">M</div>
+        <div className="WeekHeader__Column">T</div>
+        <div className="WeekHeader__Column">W</div>
+        <div className="WeekHeader__Column">T</div>
+        <div className="WeekHeader__Column">F</div>
+        <div className="WeekHeader__Column">S</div>
       </div>
     );
   }
--- packages/web/src/Calendar/index.jsx
+++ packages/web/src/Calendar/index.jsx
@@ -9,10 +9,16 @@ import Day from './Day';
 export default class Calendar extends React.Component {
   render() {
     return (
-      <div className='box'>
-        <CurrentDateHeader />
-        <WeekHeader />
+      <div className='Calendar'>
 
+        <div className="WeekHeader__Column">S</div>
+        <div className="WeekHeader__Column">M</div>
+        <div className="WeekHeader__Column">T</div>
+        <div className="WeekHeader__Column">W</div>
+        <div className="WeekHeader__Column">T</div>
+        <div className="WeekHeader__Column">F</div>
+        <div className="WeekHeader__Column">S</div>
+        { /*
         <div className='columns'>
           <div className='column'>
             <Day date={new Date()} />
@@ -35,7 +41,7 @@ export default class Calendar extends React.Component {
           <div className='column'>
             <Day date={new Date()} />
           </div>
-        </div>
+        </div> */ }
 
       </div>
     )
