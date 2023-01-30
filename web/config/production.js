20005c98f28ff338c4781394b164ec881e347d90
Alexander Paz <alexjpaz@gmail.com>
2023-01-30 15:45:22 -0500

    fixing deploy

--- web/config/production.js
+++ web/config/production.js
@@ -1,7 +1,7 @@
 module.exports = {
   "storeEndpoint": "https://b3gg00cbli.execute-api.us-east-1.amazonaws.com/prod/profile",
   "build": {
-    "TRAVIS_BUILD_NUMBER": process.env.TRAVIS_BUILD_NUMBER,
-    "TRAVIS_COMMIT": process.env.TRAVIS_COMMIT,
+    "TRAVIS_BUILD_NUMBER": process.env.BUILD_ID,
+    "TRAVIS_COMMIT": process.env.COMMIT_REF,
   }
 };
